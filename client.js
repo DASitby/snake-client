03b7c472a324c7a2984fdf24aac3aac1a6ab5b90
DASitby <davidsooley551@gmail.com>
2022-12-14 17:07:37 +0000

    add basic name functionality on connect

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function() {
+const connect = () => {
   const conn = net.createConnection({
     host: 'localhost', // IP address here,
     port: 50541, // PORT number here,
@@ -10,8 +10,14 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  //commands to run on connection
+  conn.on('connect', () => {
+    conn.write('Name: DAS');
+    console.log('successfully connected to game sever');
+  });
+
   //prints any messages sent from the server
-  conn.on("data", (message) => {
+  conn.on('data', (message) => {
     console.log(message);
   });
 
