2c6d67bcb5d7b3d0966bba027a73d268eb91e0c8
DASitby <davidsooley551@gmail.com>
2022-12-14 18:57:44 +0000

    create a variable to store active connection object, and add commands for wasd

--- input.js
+++ input.js
@@ -1,11 +1,28 @@
+//Definitions
+let connection; // Stores the active TCP connection object.
+
 //handler function to transmit the correct message on each input
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  
 };
 //Set up interface to handle user input using stdin
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
