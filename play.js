aa5c0740c435b3e53b37317651033fa530c88038
DASitby <davidsooley551@gmail.com>
2022-12-14 18:30:42 +0000

    add stdin functionality and basic user input function for quitting

--- play.js
+++ play.js
@@ -6,3 +6,18 @@ const {connect,} = require('./client');
 console.log("Connecting ...");
 connect();
 
+//handler function to transmit the correct message on each input
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+//Set up interface to handle user input using stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
